c32535cd9d4219373d7b37897b0db0fd6a9207fc
Daniele <d.gregori1@studenti.unisa.it>
2025-07-13 01:21:51 +0200

    Enhance theming support with custom dark mode variables and animations; update layout for improved structure

--- app/layout.js
+++ app/layout.js
@@ -1,30 +1,26 @@
 // app/layout.js
-
-import './globals.css'
-import {Providers} from './providers'
-import Header from '@/components/Header'
-import Footer from "@/components/Footer";
+import './globals.css';
+import { Providers } from './providers';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 
 export const metadata = {
     title: 'Daniele Gregori | Portfolio',
     description: 'Cybersecurity Specialist.',
-}
+};
 
-export default function RootLayout({children}) {
+export default function RootLayout({ children }) {
     return (
-        <html
-            lang="it"
-            suppressHydrationWarning
-        >
+        <html lang="it" suppressHydrationWarning>
         <body>
         <Providers>
             <div className="flex flex-col min-h-screen">
-                <Header/>
+                <Header />
                 {children}
-                <Footer/>
+                <Footer />
             </div>
         </Providers>
         </body>
         </html>
-    )
+    );
 }
