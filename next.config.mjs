384a661777f5e89b6a3b91e66049ad391b329d1a
Daniele <danielbat2000@gmail.com>
2026-03-17 01:33:47 +0100

    feat: add security headers to the Next.js configuration.

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,36 @@
 // next.config.mjs
 
+const securityHeaders = [
+    {
+        key: 'X-DNS-Prefetch-Control',
+        value: 'on'
+    },
+    {
+        key: 'Strict-Transport-Security',
+        value: 'max-age=63072000; includeSubDomains; preload'
+    },
+    {
+        key: 'X-Frame-Options',
+        value: 'SAMEORIGIN'
+    },
+    {
+        key: 'X-Content-Type-Options',
+        value: 'nosniff'
+    },
+    {
+        key: 'Referrer-Policy',
+        value: 'origin-when-cross-origin'
+    },
+    {
+        key: 'Permissions-Policy',
+        value: 'camera=(), microphone=(), geolocation=(), browsing-topics=()'
+    },
+    {
+        key: 'Content-Security-Policy',
+        value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://vercel.live https://va.vercel-scripts.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' blob: data: https:; font-src 'self' data: https://fonts.gstatic.com; object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'; upgrade-insecure-requests;"
+    }
+];
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // Aggiungi questa configurazione per le immagini
@@ -10,6 +41,15 @@ const nextConfig = {
                 hostname: 'api.microlink.io',
             },
         ],
+    },
+    async headers() {
+        return [
+            {
+                // Applica gli header a tutte le route
+                source: '/(.*)',
+                headers: securityHeaders,
+            },
+        ];
     },
 };
 
